ee6c55e59d9865676c0795f48ad3da402396bc93
emkeator <emkeator@gmail.com>
2017-08-03 11:54:00 -0600

    done

--- src/components/Cart/Product/Product.js
+++ src/components/Cart/Product/Product.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 import './Product.css';
 
+import {Link} from 'react-router-dom'
+
 export default function Product( { title, img, id } ) {
   return (
-    <div id="CartProduct__container">
-      <img alt="Product" src={ img } width="70px" height="70px" />
-      <span id="CartProduct__title"> { title } </span>
-    </div>
+    <Link to={`/details/${id}`} id="CartProduct__container">
+      <div id="CartProduct__container">
+        <img alt="Product" src={ img } width="70px" height="70px" />
+        <span id="CartProduct__title"> { title } </span>
+      </div>
+    </Link>
   )
 }
